9656be58c644390d3b99e3f3166ad5843a74d5c2
Kristine Kamola <kristine.kamola@gmail.com>
2024-08-26 11:29:30 +0000

    declare variables

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+//waiting for DOM to finish loading before running the game
+
+document.addEventListener('DOMContentLoaded', () {
+
+    let player = 'You';
+    let gameInUse = true;
+    let gameStatus = ['','','','','','','','',''];
+    
+    const boxes = document.querySelectorAll('box');
+    const statusShow = document.getElementById('status');
+    const reset = document.getElementById('reset');
+    
+})
+
+function boxUse(clickBox, clickIndex) {
+
+}
+
+function otherUser() {
+
+}
+
+function result() {
+
+}
+
+function boxMark() {
+
+}
+
+function restart() {
+
+}

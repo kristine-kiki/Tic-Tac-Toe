90b979a07e17767997274b8fc39c8472fc9f2ac3
Kristine Kamola <kristine.kamola@gmail.com>
2024-08-26 13:33:34 +0000

    correct game sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 //waiting for DOM to finish loading before running the game
 
-document.addEventListener('DOMContentLoaded', () {
+document.addEventListener('DOMContentLoaded', () => {
 
-    let player = 'You';
+    let player = 'X';
     let gameInUse = true;
     let gameStatus = ['','','','','','','','',''];
     
-    const boxes = document.querySelectorAll('box');
+    const boxes = document.querySelectorAll('.box');
     const reset = document.getElementById('reset');
     const statusShow = document.getElementById('status');
     
     const termsToWin = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]
+        //rows
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        //collumns
+        [1, 4, 7], [2, 5, 8], [0, 3, 6],
+        //diognal
+        [2, 4, 6], [0, 4, 8]
     ];
 
 function boxUse(clickedBox, clickedIndex) {
@@ -21,26 +26,60 @@ function boxUse(clickedBox, clickedIndex) {
 
 function otherPlayer() {
     player = player === 'X' ? 'O' : 'X';
-    statusShow.innerText = `${player}r turn!`;
+    statusShow.innerText = `${player} turn!`;
 }
 
 function result() {
     let win = false;
     for (let i = 0; i < termsToWin.length; i++) {
-        const termsToWin = termsToWin [i];
-        let x = gameStatus[termsToWin[0]];
-        let y = gameStatus[termsToWin[1]];
-        let z = gameStatus[termsToWin[2]];
-        if (x==='')
+        const [r, u, n] = termsToWin [i];
+        let kk = gameStatus[r];
+        let mm = gameStatus[u];
+        let z = gameStatus[n];
+        if (kk === ''|| mm === ''|| z === '') {
+            continue;
+        }
+        if (kk === mm && mm === z) {
+            win = true; break;
+        }
     }
+
+    if (win) {
+        statusShow.innerText = `${player} won!`;
+        gameInUse = false;
+        return;
+    }
+
+    let equal = !gameStatus.includes('');
+    if (equal) {
+        statusShow.innerText = `You both won!`;
+        gameInUse = false;
+        return;
+    }
+
+    otherPlayer();
 }
+    function boxMark(event) {
+        const clickedBox = event.target;
+        const clickedIndex = parseInt(clickedBox.getAttribute('index'));
+        if (gameStatus[clickedIndex] !== ''|| !gameInUse) {
+            return;
+        }
 
-function boxMark() {
-
+        boxUse(clickedBox, clickedIndex);
+        result();
+    
 }
 
 function restart() {
-
+    player = 'X';
+    gameInUse = true;
+    gameStatus = ['','','','','','','','',''];
+    statusShow.innerText = `${player} turn!`;
+    boxMark.forEach(box => box.innerText = '');
 }
+
+boxes.forEach(cell => cell.addEventListener('click', boxMark));
+reset.addEventListener('click', restart);
 
 });

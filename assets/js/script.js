c830122feef7afa6644f2e3e7e12fd674934390f
Kristine Kamola <kristine.kamola@gmail.com>
2024-08-26 12:15:01 +0000

    create game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,32 @@ document.addEventListener('DOMContentLoaded', () {
     let gameStatus = ['','','','','','','','',''];
     
     const boxes = document.querySelectorAll('box');
-    const statusShow = document.getElementById('status');
     const reset = document.getElementById('reset');
+    const statusShow = document.getElementById('status');
     
-})
+    const termsToWin = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]
+    ];
 
-function boxUse(clickBox, clickIndex) {
-
+function boxUse(clickedBox, clickedIndex) {
+    gameStatus[clickedIndex] = player;
+    clickedBox.innerText = player;
 }
 
-function otherUser() {
-
+function otherPlayer() {
+    player = player === 'X' ? 'O' : 'X';
+    statusShow.innerText = `${player}r turn!`;
 }
 
 function result() {
-
+    let win = false;
+    for (let i = 0; i < termsToWin.length; i++) {
+        const termsToWin = termsToWin [i];
+        let x = gameStatus[termsToWin[0]];
+        let y = gameStatus[termsToWin[1]];
+        let z = gameStatus[termsToWin[2]];
+        if (x==='')
+    }
 }
 
 function boxMark() {
@@ -31,3 +42,5 @@ function boxMark() {
 function restart() {
 
 }
+
+});

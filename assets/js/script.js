d374d50d470056c6c7204bd3bd22cb8cd7b963bd
Kristine Kamola <kristine.kamola@gmail.com>
2024-08-27 13:42:00 +0000

    add copywrite

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let playerMark = 'X';
+    //shows if game is still active
     let gameInUse = true;
+    //array showing the game layout/grid
     let gameStatus = ['','','','','','','','',''];
     
     const boxes = document.querySelectorAll('.box');
     const resetButton = document.getElementById('resetButton');
     const statusShow = document.getElementById('status');
     
+    //combinations to win
     const termsToWin = [
         //rows
         [0, 1, 2], [3, 4, 5], [6, 7, 8], 
@@ -19,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         [2, 4, 6], [0, 4, 8]
     ];
 
+//marking circle/box
 function boxUse(clickedBox, clickedIndex) {
     gameStatus[clickedIndex] = playerMark;
     clickedBox.innerText = playerMark;
@@ -42,6 +46,8 @@ function result() {
         if (player1 === ''|| player2 === ''|| z === '') {
             continue;
         }
+
+        // continue if there is empty circle/box left
         if (player1 === player2 && player2 === z) {
             winGame = true; break;
         }
@@ -67,34 +73,43 @@ function result() {
         return;
     }
 
+//switch to other player
     otherPlayer();
 }
+
+//box click event
     function boxMark(event) {
         const clickedBox = event.target;
         const clickedIndex = parseInt(clickedBox.getAttribute('index'));
+        //do nothing if the clicked circle/box is ticked or game is finished
         if (gameStatus[clickedIndex] !== ''|| !gameInUse) {
             return;
         }
-
+        //tick circle/box and check result
         boxUse(clickedBox, clickedIndex);
         result();
 }
 
+//reset game
 function restart() {
     playerMark = 'X';
     gameInUse = true;
+    //clear the game 
     gameStatus = ['','','','','','','','',''];
     statusShow.innerText = `${playerMark} turn!`;
+    //clear all X and O in circles/boxes
     boxes.forEach(box => box.innerText = '');
 }
-
+//add click event to each circle/box
 boxes.forEach(box => box.addEventListener('click', boxMark));
+//add click event to reset button
 resetButton.addEventListener('click', restart);
 
 //score functions copied from Love Math project
 function incrementX() {
-
+    //get score for X
     let oldScore = parseInt(document.getElementById('X').innerText);
+    //increment and update the score
     document.getElementById('X').innerText = ++oldScore;
 }
     function incrementO() {
